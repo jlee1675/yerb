7328ccecc7479bdc9d9321e591c55bc7e5889ea6
jlee1675 <jlee1675@gmail.com>
2019-10-26 19:29:48 -0400

    Updates

---
+++ precache-manifest.f490d60182a41892b786d580b2d2cdd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5a42b2a4de213a4b1108028c297f88",
+    "url": "/yerb/index.html"
+  },
+  {
+    "revision": "56ddfe36f81923db129b",
+    "url": "/yerb/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1ca7a1ca9e9dc205516d",
+    "url": "/yerb/static/js/2.5b2cf16e.chunk.js"
+  },
+  {
+    "revision": "56ddfe36f81923db129b",
+    "url": "/yerb/static/js/main.af5ad40a.chunk.js"
+  },
+  {
+    "revision": "6c8c363edfcc048707d7",
+    "url": "/yerb/static/js/runtime-main.fa51f91f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/yerb/static/media/logo.25bf045c.svg"
+  }
+]);
